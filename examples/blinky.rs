f30ca601638f4c11f110c7342dadd3845ec9f012
Henrik Tjäder <henrik@grepit.se>
2018-06-13 10:47:05 +0200

    Ported blinky to the updated API

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -25,7 +25,7 @@ use lpc::lpc1347::{GPIO_PORT};
 
 use lpc::gpio;
 use lpc::timers16;
-use lpc::timers16::{Timer16, MatchReg};
+use lpc::timers16::{Timer, MatchReg};
 
 // #TODO Manual start lang item
 #[start]
@@ -70,17 +70,17 @@ fn init(mut p: init::Peripherals) -> init::LateResources {
         let _ = writeln!(stdout, "Initializing...");
     }
 
-    // Initialize GPIO and set pio0_7 to output
+    // Initialize GPIO and set pio0_3 to output
     gpio::init(&p.device.SYSCON, false, false);
-    gpio::set_dir(&p.device.GPIO_PORT, gpio::Port::Port0, 7, true);
+    gpio::set_dir(&p.device.GPIO_PORT, gpio::Port::Port0, 3, true);
 
 
     // Clock 0 setup
-    timers16::reset(&p.device.CT16B0, &p.device.CT16B1, Timer16::Timer0);
-    timers16::init(&p.device.SYSCON, &mut p.core.NVIC, Timer16::Timer0);
-    timers16::set_interrupt(&p.device.CT16B0, &p.device.CT16B1, Timer16::Timer0, MatchReg::Reg0, true);
-    timers16::set_enabled(&p.device.CT16B0, &p.device.CT16B1, Timer16::Timer0, true);
-    timers16::set_match(&p.device.CT16B0, &p.device.CT16B1, Timer16::Timer0, MatchReg::Reg0, 2u16);
+    timers16::reset_t0(&p.device.CT16B0);
+    timers16::init(&p.device.SYSCON, &mut p.core.NVIC, Timer::Timer0);
+    timers16::set_interrupt_t0(&p.device.CT16B0, MatchReg::Reg0, true, false, false);
+    timers16::set_enabled_t0(&p.device.CT16B0, true);
+    timers16::set_match_t0(&p.device.CT16B0,  MatchReg::Reg0, 2u16);
 
     {
         let mut stdout = hio::hstdout().unwrap();
@@ -101,5 +101,5 @@ fn idle() -> ! {
 fn clock0_tick(_t: &mut Threshold, r: CT16B0::Resources) {
     let mut stdout = hio::hstdout().unwrap();
     let _ = writeln!(stdout, "Clock 0!");
-    r.GPIO_PORT.not[0].write(|w| w.notp7().bit(true));
+    r.GPIO_PORT.not[0].write(|w| w.notp3().bit(true));
 }
