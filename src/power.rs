6767273ce0d4f6a6b877a3ef714e32f33d8032ed
Henrik Tjäder <henrik@grepit.se>
2018-07-11 11:38:09 +0200

    Restructure and cleanup the power-save lib

--- src/power.rs
+++ src/power.rs
@@ -3,8 +3,10 @@ extern crate cortex_m;
 extern crate cortex_m_rtfm as rtfm;
 extern crate lpc1347;
 
-use lpc1347::Interrupt::{GINT0, GINT1, PIN_INT0, PIN_INT1, PIN_INT2, PIN_INT3, PIN_INT4, PIN_INT5,
-                         PIN_INT6, PIN_INT7, BOD_IRQ, USBWAKEUP, WWDT};
+use lpc1347::Interrupt::{
+    GINT0, GINT1, PIN_INT0, PIN_INT1, PIN_INT2, PIN_INT3, PIN_INT4, PIN_INT5, PIN_INT6, PIN_INT7,
+    BOD_IRQ, USBWAKEUP, WWDT,
+};
 
 /// Interrupts to be used for waking up
 pub enum WakeupInts {
@@ -36,15 +38,23 @@ pub enum WakeupInts {
     GPIO1,
 }
 
-/// Write deepsleep to the Cortex-M3
-/// Note: this is not to be used directly
-fn write_sleepdeep() {
-    let cp = lpc1347::CorePeripherals::take().unwrap();
-    let rval = cp.SCB.scr.read(); // (*cs).scr.read();
-    
-    unsafe {
-        cp.SCB.scr.write(rval | (1 << 2) | (1 << 1));
-    }
+/// Enter sleep mode
+///
+/// # Arguments
+///
+/// # Example
+/// ```
+/// fn my_task (_t: &mut Threshold, r: RESOURCE::Resources) {
+///     power::sleep(r.PMU, r.SCB);
+///
+///     wfi(); //(or rather let wfi happen in idle())
+/// }
+/// ```
+pub fn sleep(pmu: &lpc1347::PMU, scb: &mut lpc1347::SCB) {
+    // Active/Sleep is the default power mode
+    pmu.pcon.modify(|_, w| w.pm().default());
+
+    scb.clear_sleepdeep();
 }
 
 /// Enter deep-sleep mode
@@ -56,49 +66,108 @@ fn write_sleepdeep() {
 /// # Example
 /// ```
 /// fn my_task (_t: &mut Threshold, r: RESOURCE::Resources) {
-///     power::deep_sleep(r.PMU);
+///     power::deep_sleep(r.PMU, r.SYSCON, r.SCB, false, false);
 ///     wfi();
 /// }
 /// ```
 pub fn deep_sleep(
     pmu: &lpc1347::PMU,
     syscon: &lpc1347::SYSCON,
+    scb: &mut lpc1347::SCB,
     enable_bod: bool,
     enable_watchdog: bool,
 ) {
-    unsafe {
-        pmu.pcon.modify(|_, w| w.pm().bits(0x1));
+    // Activate deep-sleep
+    pmu.pcon.modify(|_, w| w.pm().deepsleep());
+
+    // Enable WD if you need timer-based wakeup
+    if enable_watchdog {
+        // Keep WWDT enabled during sleep
+        syscon.pdsleepcfg.modify(|_, w| w.wdtosc_pd().powered());
+
+        // WWDT on wakeup
+        syscon.pdawakecfg.modify(|_, w| w.wdtosc_pd().powered());
+    } else {
+        // No WWDT on wakeup
+        syscon
+            .pdsleepcfg
+            .modify(|_, w| w.wdtosc_pd().powered_down());
+        syscon
+            .pdawakecfg
+            .modify(|_, w| w.wdtosc_pd().powered_down());
     }
 
-    // Enable bod if you want low-voltage protection, WD if you nned timer-based wakeup
-    syscon.pdsleepcfg.modify(|_, w| w.bod_pd().bit(!enable_bod));
-    syscon
-        .pdsleepcfg
-        .modify(|_, w| w.wdtosc_pd().bit(!enable_watchdog));
+    // Enable bod if you want low-voltage protection
+    if enable_bod {
+        // Power BOD (Brown Out Detect)
+        syscon.pdsleepcfg.modify(|_, w| w.bod_pd().powered());
+        syscon.pdruncfg.modify(|_, w| w.bod_pd().powered());
+        syscon.pdawakecfg.modify(|_, w| w.bod_pd().powered());
+    } else {
+        // Power down BOD (Brown Out Detect)
+        syscon.pdsleepcfg.modify(|_, w| w.bod_pd().powered_down());
+        syscon.pdruncfg.modify(|_, w| w.bod_pd().powered_down());
+        syscon.pdawakecfg.modify(|_, w| w.bod_pd().powered_down());
+    }
 
-    write_sleepdeep();
+    scb.set_sleepdeep();
+    // WFI will be executed when reaching idle()
 }
 
 /// Enter power-down mode
 ///
+/// # Arguments
+/// * `enabled_bod` - Enable brown-out detector clock, this is useful if low-voltage-induced non-determinism may cause severe errors
+/// * `enabled_watchdog` - Enable watchdog clock, use this if you need to employ timed wake-ups
+///
 /// # Example
 /// ```
 /// fn my_task (_t: &mut Threshold, r: RESOURCE::Resources) {
-///     power::power_down(r.PMU);
-///     wfi();
+///     power::power_down(r.PMU, r.SYSCON, r.SCB, false, false);
 /// }
 /// ```
-pub fn power_down(pmu: &lpc1347::PMU, syscon: &lpc1347::SYSCON, bod: bool, watchdog_osc: bool) {
-    unsafe {
-        pmu.pcon.modify(|_, w| w.pm().bits(0x2));
-    }
-    syscon.pdsleepcfg.modify(|_, w| w.bod_pd().bit(!bod));
-    syscon
-        .pdsleepcfg
-        .modify(|_, w| w.wdtosc_pd().bit(!watchdog_osc));
+pub fn power_down(
+    pmu: &lpc1347::PMU,
+    syscon: &lpc1347::SYSCON,
+    scb: &mut lpc1347::SCB,
+    enable_bod: bool,
+    enable_watchdog: bool,
+) {
+    // Activate power-down
+    pmu.pcon.modify(|_, w| w.pm().powerdown());
 
-    write_sleepdeep();
-    rtfm::wfi();
+    if enable_watchdog {
+        // Keep WWDT enabled during sleep
+        syscon.pdsleepcfg.modify(|_, w| w.wdtosc_pd().powered());
+
+        // WWDT on wakeup
+        syscon.pdawakecfg.modify(|_, w| w.wdtosc_pd().powered());
+    } else {
+        // No WWDT on wakeup
+        syscon
+            .pdsleepcfg
+            .modify(|_, w| w.wdtosc_pd().powered_down());
+        syscon
+            .pdawakecfg
+            .modify(|_, w| w.wdtosc_pd().powered_down());
+    }
+
+    if enable_bod {
+        // Power BOD (Brown Out Detect)
+        syscon.pdsleepcfg.modify(|_, w| w.bod_pd().powered());
+        syscon.pdruncfg.modify(|_, w| w.bod_pd().powered());
+        syscon.pdawakecfg.modify(|_, w| w.bod_pd().powered());
+    } else {
+        // Power down BOD (Brown Out Detect)
+        syscon.pdsleepcfg.modify(|_, w| w.bod_pd().powered_down());
+        syscon.pdruncfg.modify(|_, w| w.bod_pd().powered_down());
+        syscon.pdawakecfg.modify(|_, w| w.bod_pd().powered_down());
+    }
+
+    // Required for power_down mode
+    scb.set_sleepdeep();
+
+    // WFI will be executed when reaching idle()
 }
 
 /// Enter deep power-down mode
@@ -110,22 +179,27 @@ pub fn power_down(pmu: &lpc1347::PMU, syscon: &lpc1347::SYSCON, bod: bool, watch
 ///     ...
 ///
 ///     // Power down
-///     power::power_down(r.PMU);
-///     wfi();
+///     power::deep_power_down(r.PMU, r.SYSCON, r.SCB, false, false);
 /// }
 /// ```
-pub fn deep_power_down(pmu: &lpc1347::PMU) {
-    // Remember to clear the nodpd bit!pmu.pcon.modify(|_, w| w.nodpd().bit(false));
-    unsafe {
-        pmu.pcon.modify(|_, w| w.pm().bits(0x3));
-    }
-    write_sleepdeep();
+/// #TODO Brownout detection?
+pub fn deep_power_down(pmu: &lpc1347::PMU, scb: &mut lpc1347::SCB) {
+    // Remember to clear the nodpd bit if dpd is wanted!
+    // eg: pmu.pcon.modify(|_, w| w.nodpd().bit(false));
+    // Activate power-down
+    pmu.pcon.modify(|_, w| w.pm().deeppowerdown());
+    scb.set_sleepdeep();
+    // WFI will be executed when reaching idle()
 }
 
 /// Set wakeup interrupts
 ///
 ///
-pub fn set_wakeup_interrupt(syscon: &lpc1347::SYSCON, nvic: &mut lpc1347::NVIC, interrupt: WakeupInts) {
+pub fn set_wakeup_interrupt(
+    syscon: &lpc1347::SYSCON,
+    nvic: &mut lpc1347::NVIC,
+    interrupt: WakeupInts,
+) {
     match interrupt {
         WakeupInts::Pin0 => {
             syscon.starterp0.modify(|_, w| w.pint0().bit(true));
